799d240385592ba424b7530d47692ba246eef0d6
cubancubano <favouradepoju30@gmail.com>
2022-09-13 19:57:37 -0700

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+	{
+	int i = 0;
+	char name[] = "_putchar";
+
+
+		_putchar(name);
+		_putchar('\n');
+	return (0);
+}
